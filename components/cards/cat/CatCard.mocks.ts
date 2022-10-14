2dbc5573d26c987c27555ccfd356826aef975d5c
akidari21 <akidari21@gmail.com>
2022-10-14 19:06:38 +0900

    build: add story card

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  tag: '고냥순',
+  title: `냥이의 새로운 점`,
+  body: '냥이는 독립적인 것 같지만 모든 생명은 마음이 있고 감성이 있다',
+  author: '냥주인',
+  time: '2h ago',
+};
+
+export const mockCatCardProps = {
+  base,
+};
